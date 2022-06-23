9ae4c6d783be5277a56cb1459f3b9cccdeea4820
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-23 16:31:13 +0800

    created method to create subjects tgt with grades for the subjects

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,5 +1,41 @@
 class SubjectsController < ApplicationController
   def index
-    @subjects = current_user.subjects
+    @listed_subjects = current_user.subjects.where(listed: true)
+    @unlisted_subjects = current_user.subjects.where(listed: false)
+  end
+
+  def bulk
+    subject_name = params["subject"]
+    if subject_name.nil? || subject_name == "Select subject"
+      flash[:error] = "You did not select a subject"
+      redirect_to subjects_path
+    end
+    new_subject = Subject.new(name: subject_name, user: current_user)
+
+    if Grade::GRADES.any? { |grade| params[grade] == "on" }
+      new_subject.save!
+    else
+      flash[:error] = "You did not select any grades"
+      redirect_to subjects_path
+    end
+
+    Grade::GRADES.each { |grade| create_grades(grade, new_subject) }
+    # List the subject created
+    new_subject.list!
+    redirect_to subjects_path
+  end
+
+  private
+
+  def create_grades(grade, subject)
+    if params[grade] == "on"
+      rate = params["#{grade} Rate"]
+      if rate.nil?
+        flash[:error] = "You did not enter an hourly rate for #{grade}"
+        redirect_to subjects_path
+      end
+      grade = Grade.new(name: grade, hourly_rate: rate.to_f, subject: subject)
+      grade.save
+    end
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,7 +1,11 @@
 class Subject < ApplicationRecord
-  SUBJECTS = %w[A.Math E.Math English Chinese Malay Tamil Geography History Social_Studies Physics Biology Chemistry]
+  SUBJECTS = ['A.Math', 'E.Math', 'English', 'Chinese', 'Malay', 'Tamil', 'Geography', 'History', 'Social Studies', 'Physics', 'Biology', 'Chemistry']
 
   belongs_to :user
   has_many :grades, dependent: :destroy
   validates :name, presence: true, inclusion: { in: SUBJECTS }
+
+  def list!
+    grades.any?
+  end
 end
