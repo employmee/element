7cbd7f8e50fd43870027c3b26b2869019fc61182
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-25 16:34:35 +0800

    completed method for user to bulk delete availabilities slots by clicking on them

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -9,8 +9,17 @@ class AvailabilitiesController < ApplicationController
     else
       start_date = params.fetch(:start_date, Date.today).to_date
       @availability_slot = Availability.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
-      @schedule = Schedule.new
-      render 'schedules/index'
+      @schedules = current_user.schedules
+      redirect_to schedules_path
+    end
+  end
+
+  def destroy
+    @availability = Availability.find(params[:id])
+    @availability.destroy
+
+    respond_to do |format|
+      format.json {{status: "ok"}}
     end
   end
 
--- app/javascript/controllers/availability_controller.js
+++ app/javascript/controllers/availability_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "stimulus"
-import Sortable from "sortablejs"
 import { csrfToken } from "@rails/ujs";
 
 
 export default class extends Controller {
-  static targets = [  ]
+  static targets = [ "deleteBtn", "confirmBtn", "deleteMode" ]
   // static values = {
   //   to_unlist_id: String,
   //   to_list_id: String,
@@ -15,17 +14,74 @@ export default class extends Controller {
   }
 
   displayScheduleForm() {
-    const form = document.getElementById("overlay-left")
+    const form = document.getElementById("overlay-left");
     form.style.display = "block";
   }
 
   hideScheduleForm() {
-    const form = document.getElementById("overlay-left")
+    const form = document.getElementById("overlay-left");
+    form.style.display = "none";
+  }
+  hideAvailForm() {
+    const form = document.getElementById("overlay-right");
     form.style.display = "none";
   }
 
   displayAvailForm() {
-    const form = document.getElementById("overlay-right")
+    const form = document.getElementById("overlay-right");
     form.style.display = "block";
+  }
+
+  deleteMode() {
+    let deleteBtn = this.deleteBtnTarget;
+    const confirmBtn = this.confirmBtnTarget;
+    confirmBtn.style.display = "block";
+    deleteBtn.outerHTML = `<button data-availability-target="deleteMode" data-action="click->availability#HideDeleteMode" class="btn-outline-red mx-2">delete mode</button>`
+  }
+  HideDeleteMode() {
+    let deleteMode = this.deleteModeTarget;
+    const confirmBtn = this.confirmBtnTarget;
+    confirmBtn.style.display = "none";
+    deleteMode.outerHTML = `<button data-availability-target="deleteBtn" data-action="click->availability#deleteMode" class="btn-square center mx-2" style="background-color:#A63D40;"><i class="fa-solid fa-minus" style="color:white"></i></button>`
+
+    // Turn back all selected slots to normal slots without the highlighted style
+    const selectedSlots = document.querySelectorAll(".month_selected")
+    selectedSlots.forEach(slot => slot.className = "month_available")
+  }
+
+  highlightSlot(event) {
+    const confirmBtn = this.confirmBtnTarget;
+    // Only highlight if in delete mode
+    if (confirmBtn.style.display === "block") {
+      console.log(event.path)
+      if (event.path.find(element => element.className == "month_available" )) {
+        const slot = event.path.find(element => element.className.includes("month_available"))
+        slot.className = "month_selected"
+      } else if (event.path.find(element => element.className == "month_selected")) {
+        const slot = event.path.find(element => element.className.includes("month_selected"))
+        slot.className = "month_available"
+      }
+    }
+  }
+
+  deleteSelected(event) {
+    event.preventDefault();
+    let selected = document.getElementsByClassName("month_selected")
+    let selected_ids = []
+    Array.from(selected).forEach(ele => selected_ids.push(ele.id));
+    console.log(selected_ids)
+    selected_ids.forEach(id => {
+      console.log(id)
+      fetch(`/availabilities/${id}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRF-Token': csrfToken()
+        }
+      })
+      .then(res => res.json())
+      })
+    location.reload();
   }
 }
