14c7f62190ae0e1941913736a14c3f401d6933c4
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-27 16:59:10 +0800

    added validation for booking status to be unique on avail. i.e avail cannot have 2 bookings where status is pending

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,6 +5,7 @@ class Booking < ApplicationRecord
 
   STATUS = %w[pending confirmed cancelled completed]
   validates :status, inclusion: { in: STATUS }
+  validates :status, uniqueness: { scope: :availability }
 
   def check_and_turn_completed
     self.status = "completed" if (self.status == "confirmed" && Time.now > availability.end_time)
