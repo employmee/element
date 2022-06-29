5eddfde814825ca90095e209969843c9e4d1c7ec
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-30 01:36:11 +0800

    changed calendar param to include start of day and end of day

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -3,7 +3,7 @@ class SchedulesController < ApplicationController
     # parameters for calendar
     start_date = params.fetch(:start_date, Date.today).to_date
     user_id = current_user.id
-    @month_availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week).where(user_id: user_id)
+    @month_availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week.beginning_of_day..start_date.end_of_month.end_of_week.end_of_day).where(user_id: user_id)
     # use next line for week view instead of month view for calendar
     # @availability_slot = Availability.where(start_time: start_date.beginning_of_week.beginning_of_day..start_date.end_of_week.end_of_day).where(user_id: user_id)
 
