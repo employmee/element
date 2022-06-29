f31bcb4ee32cf5d3648fdeb8b6c9e84780b72653
wanxin pua <wanxinpua@yahoo.com.sg>
2022-06-30 00:51:17 +0800

    added redirect to the wrong controller action. moved from index to show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,35 +3,35 @@ class UsersController < ApplicationController
 
   def index
     @user = User.new
-    if (@user.first_name.nil? || @user.listed_subjects.empty) && current_user != @user
-      redirect_to users_path
+    # Select onl teachers with grades that have grades listed
+    if params[:query].present?
+      #@teachers = User.search_for_teacher(params[:query])
+      sql_query = " \
+      users.role = 'Teacher' AND subjects.listed = true AND \
+      (users.first_name ILIKE :query \
+        OR users.last_name ILIKE :query \
+        OR users.location ILIKE :query \
+        OR subjects.name ILIKE :query \
+        OR grades.name ILIKE :query) \
+      "
+      @teachers = User.joins(:subjects, :grades).where(sql_query, query: "%#{params[:query]}%").uniq
     else
-      # Select onl teachers with grades that have grades listed
-      if params[:query].present?
-        #@teachers = User.search_for_teacher(params[:query])
-        sql_query = " \
-        users.role = 'Teacher' AND subjects.listed = true AND \
-        (users.first_name ILIKE :query \
-          OR users.last_name ILIKE :query \
-          OR users.location ILIKE :query \
-          OR subjects.name ILIKE :query \
-          OR grades.name ILIKE :query) \
-        "
-        @teachers = User.joins(:subjects, :grades).where(sql_query, query: "%#{params[:query]}%").uniq
-      else
-        @teachers = User.listed_teachers
-      end
+      @teachers = User.listed_teachers
     end
   end
 
   def show
-    @booking = Booking.new
-    @review = Review.new
-    @user_bookings_added = []
-    # This section is for the calendar view
-    start_date = params.fetch(:start_date, Date.today).to_date
-    #@availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-    @availability_slot = Availability.where(start_time: Time.now..start_date.end_of_week.end_of_day).where(user_id: @user.id)
+    if (@user.first_name.nil? || @user.listed_subjects.empty) && current_user != @user
+      redirect_to users_path
+    else
+      @booking = Booking.new
+      @review = Review.new
+      @user_bookings_added = []
+      # This section is for the calendar view
+      start_date = params.fetch(:start_date, Date.today).to_date
+      #@availability_slot = Availability.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+      @availability_slot = Availability.where(start_time: Time.now..start_date.end_of_week.end_of_day).where(user_id: @user.id)
+    end
   end
 
   private
