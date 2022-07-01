ae0a6393d2f7bad59160ac1c47050765584bc111
wanxin pua <wanxinpua@yahoo.com.sg>
2022-07-01 17:07:27 +0800

    redirect to bookings path for teachers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.role == "Teacher"
       if current_user.first_name.present?
-        subjects_path
+        bookings_path
       else
         edit_user_registration_path(anchor: 'edit-profile')
       end
