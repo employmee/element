9c61cda8a12dc89d2f1717afa30a0e8b5f806681
wanxin pua <wanxinpua@yahoo.com.sg>
2022-07-01 19:21:27 +0800

    removed review rating validation. validation is handled front end

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
 
   validates :content, :rating, presence: true
-  validates :rating, uniqueness: { scope: :student_id, message: "You have already entered a review for this teacher" }
+  # validates :rating, uniqueness: { scope: :student_id, message: "You have already entered a review for this teacher" }
 end
